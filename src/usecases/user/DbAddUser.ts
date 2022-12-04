650d5b21c3eb47f636bd10371f7fe65f1bf6a0f4
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-12-04 13:31:15 -0300

    Ensure validation field is correct

--- src/usecases/user/DbAddUser.ts
+++ src/usecases/user/DbAddUser.ts
@@ -1,5 +1,6 @@
 import { User } from '@prisma/client';
 import { IUsersRepository } from '../../repositories';
+import { AddUserValidation } from '../../validations';
 
 class DbAddUser {
     constructor(private readonly usersRepository: IUsersRepository) {}
@@ -8,6 +9,7 @@ class DbAddUser {
         data: Omit<User, 'id' | 'createdAt' | 'updatedAt'>
     ): Promise<User> {
         const { email } = data;
+        await AddUserValidation(data);
         const userAlreadyExists = await this.usersRepository.findByEmail(email);
         if (userAlreadyExists !== null) {
             throw new Error('Email already exists.');
