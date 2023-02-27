b6162c521665309e6d0ec8de4a4a1aa330298171
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-02-27 17:46:14 -0300

    add public avatars

--- src/app/config/routes.ts
+++ src/app/config/routes.ts
@@ -1,10 +1,11 @@
-import { Express, Router } from 'express';
+import express, { Express, Router } from 'express';
 import { EvaluationRoutes, MovieRoutes, UserRoutes } from '../routes';
 import { TrendingRoutes } from '../routes/TrendingRoutes';
 
 const routes = (app: Express) => {
     const router = Router();
     app.use('/api', router);
+    app.use('/public', express.static('public'));
     UserRoutes(router);
     MovieRoutes(router);
     EvaluationRoutes(router);
