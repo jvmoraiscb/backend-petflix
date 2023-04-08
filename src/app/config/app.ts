b2627e2c01dfe787291340d84ab4d07ac34c7ce9
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-04-08 16:59:51 -0300

    add cors

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,9 +1,16 @@
 import express, { Express, json } from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const setupApp = async (): Promise<Express> => {
     const app = express();
     app.use(json());
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+        app.use(cors());
+        next();
+    });
     routes(app);
     return app;
 };
