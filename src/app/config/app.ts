ceedf75f38392a58be7d61bc5aaa8552cddd820c
Luiz Felipe Machado <luizfelmach@gmail.com>
2023-05-18 13:49:42 -0300

    :zap: update cors

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -4,13 +4,8 @@ import { routes } from './routes';
 
 const setupApp = async (): Promise<Express> => {
     const app = express();
+    app.use(cors());
     app.use(json());
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-        app.use(cors());
-        next();
-    });
     routes(app);
     return app;
 };
